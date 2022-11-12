c538f4c4188de0b90faf32d65a2495bfc75425a1
Brandon <brandon.dicicco@gmail.com>
2022-11-12 12:54:36 -0500

    format

--- PracticeTests/C#/hackerRankFizbuzzPracticeTest.cs
+++ PracticeTests/C#/hackerRankFizbuzzPracticeTest.cs
@@ -1,20 +1,20 @@
- for(int i =1; i <=n; i++)
-        {
-         var returnStr = "";
-            if (i % 3 == 0)
-            {
-                returnStr += "Fizz";
-            }
-            if (i % 5 == 0)
-            {
-                returnStr += "Buzz";
-            }
-           
-            if (String.IsNullOrEmpty(returnStr))
-            {
-                Console.WriteLine(i.ToString());
-            }
-            else{
-              Console.WriteLine(returnStr);  
-            }
-        } 
+for(int i =1; i <=n; i++)
+{
+    var returnStr = "";
+    if (i % 3 == 0)
+    {
+        returnStr += "Fizz";
+    }
+    if (i % 5 == 0)
+    {
+        returnStr += "Buzz";
+    }
+    
+    if (String.IsNullOrEmpty(returnStr))
+    {
+        Console.WriteLine(i.ToString());
+    }
+    else{
+        Console.WriteLine(returnStr);  
+    }
+}
